7f2ef18e4455e61c84f8db1d994c70227c2ccc39
frankpagan <frank@cocreate.app>
2021-02-14 23:55:40 +0000

    clean tree

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -9,7 +9,7 @@ module.exports = {
     sources: [{
             path: "./docs/index.html",
             collection: "files",
-            document_id: "",
+            document_id: "6029b6a3e979fb17407e25dc",
             key: "html",
             data:{
                 name: "Twilio Doc",
@@ -19,10 +19,10 @@ module.exports = {
    
     crud: [{
             collection: "routes",
-            document_id: "",
+            document_id: "6029b6a3e979fb17407e25db",
             data:{
                 collection: "files",
-                document_id: "",
+                document_id: "6029b6a3e979fb17407e25dc",
                 name: "html",
                 domains: ["cocreate.app", "server.cocreate.app", "ws.cocreate.app"],
                 route: "/docs/twilio",
