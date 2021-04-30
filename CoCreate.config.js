4f826b34480fc4ac68479b47825628ea2c96a5cf
frank <frank@cocreate.app>
2021-04-30 04:26:16 +0000

    add src

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -10,7 +10,7 @@ module.exports = {
             "path": "./docs/index.html",
             "collection": "files",
             "document_id": "6029b6a3e979fb17407e25dc",
-            "key": "html",
+            "key": "src",
             "data":{
                 "name": "twilio Doc",
                 "domains": ["cocreate.app", "server.cocreate.app", "ws.cocreate.app"],
