3de68ffdfcd30c6fa9a307c91ec22edeb2f2203f
frankpagan <frank@cocreate.app>
2023-09-17 19:11:38 -0600

    fix:  Add path and pathname

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -8,17 +8,16 @@ module.exports = {
             "object": {
                 "_id": "6029b6a3e979fb17407e25dc",
                 "name": "index.html",
-                "path": "/docs/twilio/index.html",
+                "path": "/docs/twilio",
+                "pathname": "/docs/twilio/index.html",
                 "src": "{{./docs/index.html}}",
                 "host": [
                     "*",
                     "general.cocreate.app"
                 ],
-                "directory": "/docs/twilio",
-                "parentDirectory": "{{parentDirectory}}",
+                "directory": "twilio",
                 "content-type": "{{content-type}}",
-                "public": "true",
-                "website_id": "644d4bff8036fb9d1d1fd69c"
+                "public": "true"
             }
         }
     ]
